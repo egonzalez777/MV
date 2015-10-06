4c981b5ed2a2b8dc2db38dafd889b048ab77eac1
Eric Gonzalez <eric.gonz.gomez@gmail.com>
2015-10-05 23:06:50 -0700

    Added socket.io implementation.

---
+++ mv/public/javascripts/global.js
@@ -0,0 +1,24 @@
+(function () {
+	var socket = io.connect();
+	console.log(socket);
+
+	$('body').on('click', 'button.meh', function (evt) {
+		socket.emit('updated', 'Meh');;
+		console.log('boo');
+	});
+
+	socket.on('broadcasted_update', function (d) {
+		console.log(d);
+		var el = $('<div class="object" />').css({'width': '300px', 'height': '150px', 'background': '#ff00ff', 'color': 'blue'});
+		$('body').append(el);
+		el.html('<p>MOOOOO WHATSUP');
+		el.fadeOut();
+		setTimeout(function () {
+			el.fadeIn();
+			setTimeout(function () {
+				el.fadeOut();
+			}, 20000);
+		}, 1000);
+	});
+	
+})();
